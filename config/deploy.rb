4db47b0b1b6a6c1984d1cd32b445e975e492bb9e
a-fuenzalida <a.fuenzalida1494@gmail.com>
2020-07-20 19:03:35 -0400

    prueba 6

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,7 +27,7 @@ set :use_sudo, false
 set :linked_files, %w[config/database.yml config/secrets.yml]
 set :linked_dirs, %w[log tmp public/system public/assets public/ckeditor_assets]
 
-set :keep_releases, 5
+set :keep_releases, 3
 
 set :local_user, ENV["USER"]
 
