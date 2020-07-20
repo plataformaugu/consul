8d1dd616c0cb1fe1bfe9f8332193ceb0c0eaa381
a-fuenzalida <a.fuenzalida1494@gmail.com>
2020-07-20 17:32:36 -0400

    prueba 3

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-set :branch, ENV["branch"] || :master
+set :branch, ENV["branch"] || :ugu
 
 server deploysecret(:server1), user: deploysecret(:user), roles: %w[web app db importer cron background]
 #server deploysecret(:server2), user: deploysecret(:user), roles: %w(web app db importer cron background)
