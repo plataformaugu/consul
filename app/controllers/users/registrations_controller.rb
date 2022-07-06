0cee596e2584e4b6c0fa82e7955307b2f66e780a
afuenzalida <a.fuenzalida1494@gmail.com>
2022-07-05 20:27:57 -0400

    safe registro civil response data

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -35,20 +35,13 @@ class Users::RegistrationsController < Devise::RegistrationsController
           redirect_to root_path
         end
       else
-        # born_data = registro_civil_request(response[:data]['rut'], 'certificado-nacimiento')
-        # profession_data = registro_civil_request(response[:data]['rut'], 'informacion-profesion')
-        # home_data = registro_civil_request(response[:data]['rut'], 'informacion-domicilio')
+        born_data = registro_civil_request(response[:data]['rut'], 'certificado-nacimiento')
+        profession_data = registro_civil_request(response[:data]['rut'], 'informacion-profesion')
+        home_data = registro_civil_request(response[:data]['rut'], 'informacion-domicilio')
 
-        # if born_data.nil? or profession_data.nil? or home_data.nil? or born_data.fetch('message', '') == 'Server Error' or profession_data.fetch('message', '') == 'Server Error' or home_data.fetch('message', '') == 'Server Error'
-        # if profession_data.nil? or home_data.nil? or profession_data.fetch('message', '') == 'Server Error' or home_data.fetch('message', '') == 'Server Error'
-        #   flash[:alert] = "Ocurrió un error inesperado. Inténtalo más tarde."
-        #   redirect_to new_user_registration_path
-        #   return
-        # end
-
-        # born_data = born_data.fetch('CertificadoNacimiento', {})
-        # profession_data = profession_data.fetch('datosPersona', {}).fetch('datosProfesion', {})
-        # home_data = home_data.fetch('datoPersona', {})
+        born_data = !born_data.nil? ? born_data.fetch('CertificadoNacimiento', {}) : {}
+        profession_data = !profession_data.nil? ? profession_data.fetch('datosPersona', {}).fetch('datosProfesion', {}) : {}
+        home_data = !home_data.nil? ? home_data.fetch('datoPersona', {}) : {}
 
         build_resource({
           username: "#{response[:data]['nombre'].downcase[0]}#{response[:data]['apellido_paterno'].downcase}#{response[:data]['rut'][..4]}",
@@ -56,10 +49,10 @@ class Users::RegistrationsController < Devise::RegistrationsController
           first_name: response[:data]['nombre'],
           last_name: response[:data]['apellido_paterno'],
           maiden_name: response[:data]['apellido_materno'],
-          # date_of_birth: !born_data.fetch('fechaNacimiento', nil).nil? ? Date.strptime(born_data['fechaNacimiento'], '%Y-%m-%d') : nil,
-          # civil_status: home_data.fetch('estadoCivil', nil),
-          # nationality: born_data.fetch('nacionalidadNacimiento', '').titleize,
-          # profession: (profession_data.is_a?(Hash) && !profession_data.fetch('tituloProfesional', nil).nil?) ? profession_data['tituloProfesional'].titleize : nil
+          date_of_birth: !born_data.fetch('fechaNacimiento', nil).nil? ? Date.strptime(born_data['fechaNacimiento'], '%Y-%m-%d') : nil,
+          civil_status: home_data.fetch('estadoCivil', nil),
+          nationality: born_data.fetch('nacionalidadNacimiento', '').titleize,
+          profession: (profession_data.is_a?(Hash) && !profession_data.fetch('tituloProfesional', nil).nil?) ? profession_data['tituloProfesional'].titleize : nil
         })
 
         @countries = get_countries
