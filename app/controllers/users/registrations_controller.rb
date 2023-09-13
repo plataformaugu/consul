a50981499c769dc0a70becca334a90837f4bb326
Andres Fuenzalida <andres@MacBook-Pro-de-Andres.local>
2023-09-12 22:22:57 -0300

    add user data request to neighborhood directory in registration

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -88,6 +88,13 @@ class Users::RegistrationsController < Devise::RegistrationsController
         resource.sector = Sector.where(name: "C#{sector_data['sector']}").first
         resource.lat = sector_data['lat'].gsub(',', '.').to_f
         resource.long = sector_data['long'].gsub(',', '.').to_f
+
+        if resource.comuna == 'Las Condes'
+          send_user_data_to_neighborhood_directory(
+            resource,
+            sector_data['id']
+          )
+        end
       end
     end
 
@@ -161,7 +168,8 @@ class Users::RegistrationsController < Devise::RegistrationsController
           return {
             "sector" => sector_data['cod_unidadvecinal'],
             "lat" => sector_data['str_latitud'],
-            "long" => sector_data['str_longitud']
+            "long" => sector_data['str_longitud'],
+            "id" => sector_data['id']
           }
         end
       else
@@ -277,6 +285,58 @@ class Users::RegistrationsController < Devise::RegistrationsController
         result = JSON.parse(response.body)
         return result
       rescue
+        return nil
+      end
+    end
+
+    def send_user_data_to_neighborhood_directory(user, address_id)
+      begin
+        logger.info("Sending user data to neighborhood directory...")
+
+        uri = URI("https://bus-datos.lascondes.cl/api/maestros/vecinos/")
+
+        https = Net::HTTP.new(uri.host, uri.port)
+        https.use_ssl = true
+        https.read_timeout = 5
+
+        civil_status = 'SOLTERO'
+
+        if user.civil_status
+          if user.civil_status == 'C'
+            civil_status = 'CASADO'
+          elsif user.civil_status == 'V'
+            civil_status = 'VIUDO'
+          end
+        end
+
+        payload = {
+          "rut": user.document_number, 
+          "nombre": user.first_name,
+          "ap_paterno": user.last_name,
+          "ap_materno": user.maiden_name,
+          "email": user.email,
+          "celular": user.phone_number.to_i, 
+          "cod_comuna": "13114", 
+          "origen": "PARTICIPACIÓN CIUDADANA", 
+          "sexo": user.gender.upcase, 
+          "estado_civil": civil_status, 
+          "fc_nacimiento": user.date_of_birth.strftime('%Y-%m-%d'), 
+          "id_direccion": address_id
+        }
+
+        request = Net::HTTP::Post.new(uri.path, 'Content-Type' => 'application/json')
+        request["Authorization"] = "Bearer A8Vq8HmOepf38i38i7D95RkF3kxhmeSOVlItK4rFim12tK4rFim12diVun3aHe9k9Ll0"
+        request.body = JSON.dump(payload)
+        response = https.request(request)
+
+        if response.kind_of? Net::HTTPSuccess
+          return nil
+        else
+          raise "API response error: #{response.body}"
+        end
+      rescue => e
+        logger.error("[CUSTOM ERROR] Failed to send user data to neighborhood directory for user #{user.document_number}")
+        logger.error e.message
         return nil
       end
     end
