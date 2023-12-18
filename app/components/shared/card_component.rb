49ca674be016ab8ab99cea07e8539e22dbe28f96
afuenzalida <a.fuenzalida1494@gmail.com>
2023-12-18 08:41:12 -0300

    fix details

--- app/components/shared/card_component.rb
+++ app/components/shared/card_component.rb
@@ -1,10 +1,11 @@
 class Shared::CardComponent < ApplicationComponent
-  def initialize(instance, title, image, description, alert, extra_info = nil)
+  def initialize(instance, title, image, description, alert, extra_info = nil, footer_text = nil)
     @instance = instance
     @title = title
     @image = image
     @description = description
     @alert = alert
     @extra_info = extra_info
+    @footer_text = footer_text
   end
 end
