eab313d1f78d574f19207037bd58da5cf77490f2
afuenzalida <a.fuenzalida1494@gmail.com>
2025-01-06 08:30:45 -0300

    Add dynamic path label to section component and include budgets in welcome index

--- app/components/welcome/section_component.rb
+++ app/components/welcome/section_component.rb
@@ -1,6 +1,7 @@
 class Welcome::SectionComponent < ApplicationComponent
-  def initialize(title, path = nil)
+  def initialize(title, path = nil, path_label = 'Ver todas')
     @title = title
     @path = path
+    @path_label = path_label
   end
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -11,11 +11,22 @@ class WelcomeController < ApplicationController
     @headers = Widget::Card.header.all
     @proposals = Proposal.published.order(created_at: :desc).limit(3)
     @polls = Poll.created_by_admin.not_budget.visible.order(created_at: :desc).limit(3)
+
     hidden_survey_ids = [10]
     @surveys = Survey.published.where.not(id: hidden_survey_ids).order(created_at: :desc).limit(3)
+
     @debates = Debate.published.order(created_at: :desc).limit(3)
     @events = Event.order(created_at: :desc).limit(3)
     @cards = Widget::Card.body
+
+    fake_budget = Survey.published.where(id: 1).first
+    third_last_budgets = Budget.published.order(created_at: :desc).limit(3).to_a
+
+    if fake_budget.present?
+      third_last_budgets.append(fake_budget)
+    end
+
+    @budgets = third_last_budgets.sort_by(&:created_at).reverse[..2]
   end
 
   def welcome
