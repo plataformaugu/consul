ea00290685e653c4997bfa93ae2241b9bbb5b193
afuenzalida <a.fuenzalida1494@gmail.com>
2025-01-16 12:01:56 -0300

    enable SSL for HTTP requests in LoBarnecheaApi and disable certificate verification

--- lib/lo_barnechea_api.rb
+++ lib/lo_barnechea_api.rb
@@ -17,7 +17,8 @@ class LoBarnecheaApi
     uri = URI("#{URL}/prd/api/v1/maestrocalles/detalle/calles/")
 
     https = Net::HTTP.new(uri.host, uri.port)
-    https.use_ssl = false
+    https.use_ssl = true
+    https.verify_mode = OpenSSL::SSL::VERIFY_NONE
     https.read_timeout = 15
 
     request = Net::HTTP::Post.new(uri.path, 'Content-Type' => 'application/json')
@@ -43,7 +44,8 @@ class LoBarnecheaApi
       uri = URI("#{URL}/prd/api/v1/maestrocalles/calles")
 
       https = Net::HTTP.new(uri.host, uri.port)
-      https.use_ssl = false
+      https.use_ssl = true
+      https.verify_mode = OpenSSL::SSL::VERIFY_NONE
       https.read_timeout = 15
 
       request = Net::HTTP::Post.new(uri.path, 'Content-Type' => 'application/json')
