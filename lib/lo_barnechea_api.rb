9d0dd124d263f6e2fd740079e076a3fb35483354
afuenzalida <a.fuenzalida1494@gmail.com>
2026-06-26 21:18:59 -0400

    refactor: error handling and logging in LoBarnecheaApi methods

--- lib/lo_barnechea_api.rb
+++ lib/lo_barnechea_api.rb
@@ -3,7 +3,7 @@ class LoBarnecheaApi
 
   def get_street_names
     street_names = Rails.cache.fetch('street_names', expires_in: 24.hours) do
-      results = fetch_street_names
+      fetch_street_names
     end
 
     if street_names.empty?
@@ -25,18 +25,28 @@ class LoBarnecheaApi
     request["Content-Type"] = "application/json"
     request.body = JSON.dump({"calle": street_name})
 
-    response = https.request(request)
-    results = []
+    begin
+      response = https.request(request)
 
-    if response.kind_of? Net::HTTPSuccess
-      parsed_response = JSON.parse(response.body)
-
-      if parsed_response.key?('direcciones')
-        results = parsed_response['direcciones'].map { |record| record['_source']['numero'] }.uniq
+      if response.kind_of? Net::HTTPSuccess
+        parsed_response = JSON.parse(response.body)
+        if parsed_response.key?('direcciones')
+          return parsed_response['direcciones'].map { |record| record['_source']['numero'] }.uniq
+        end
+      else
+        log_error(
+          method: __method__,
+          endpoint: uri,
+          request_body: { calle: street_name },
+          status: response.code,
+          response_body: response.body
+        )
       end
+    rescue StandardError => e
+      log_exception(method: __method__, endpoint: uri, exception: e)
     end
 
-    return results
+    []
   end
 
   private
@@ -50,17 +60,49 @@ class LoBarnecheaApi
 
       request = Net::HTTP::Post.new(uri)
 
-      response = https.request(request)
-      results = []
+      begin
+        response = https.request(request)
 
-      if response.kind_of? Net::HTTPSuccess
-        parsed_response = JSON.parse(response.body)
-
-        if parsed_response.key?('direcciones')
-          results = parsed_response['direcciones']
+        if response.kind_of? Net::HTTPSuccess
+          parsed_response = JSON.parse(response.body)
+          if parsed_response.key?('direcciones')
+            return parsed_response['direcciones']
+          end
+        else
+          log_error(
+            method: __method__,
+            endpoint: uri,
+            request_body: nil,
+            status: response.code,
+            response_body: response.body
+          )
         end
+      rescue StandardError => e
+        log_exception(method: __method__, endpoint: uri, exception: e)
       end
 
-      return results
+      []
+    end
+
+    def log_error(method:, endpoint:, request_body:, status:, response_body:)
+      Rails.logger.error(
+        "[LoBarnecheaApi] Request failed | " \
+        "method=#{method} " \
+        "endpoint=#{endpoint} " \
+        "request_body=#{request_body.inspect} " \
+        "status=#{status} " \
+        "response_body=#{response_body}"
+      )
+    end
+
+    def log_exception(method:, endpoint:, exception:)
+      Rails.logger.error(
+        "[LoBarnecheaApi] Request exception | " \
+        "method=#{method} " \
+        "endpoint=#{endpoint} " \
+        "exception=#{exception.class} " \
+        "message=#{exception.message} " \
+        "backtrace=#{exception.backtrace&.first(5)&.join(' | ')}"
+      )
     end
 end
